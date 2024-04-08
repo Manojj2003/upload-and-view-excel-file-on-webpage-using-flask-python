18170f1edd58852e32e29fe4aac1d0320d59bf14
Manojj2003 <140328363+Manojj2003@users.noreply.github.com>
2024-04-09 02:55:17 +0530

    upload & view the excel file in webpage using flask
    upload the excel file to specific local folder directory in & view the excel file in webpage using flask  python

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+app.config["UPLOAD_FOLDER"]="static/excel"
+@app.route("/",methods=['GET','POST'])
+def upload():
+    if request.method=="POST":
+        upload_excel=request.files['upload_excel']
+        if upload_excel.filename!='':
+            filepath=os.path.join(app.config["UPLOAD_FOLDER"],upload_excel.filename)
+            upload_excel.save(filepath)
+            data = pd.read_excel(upload_excel)
+            return render_template ("ExcelFile.html",data.to_html(index=False).replace('<th>', '<th style="text-align:center">'))
+    return render_template("uploadExcel.html")    
+    
+if __name__ == '__main__':
+    app.run(debug=True) 
+
+
+
+
